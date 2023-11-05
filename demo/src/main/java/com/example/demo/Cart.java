3519bea1eba8b17e37b6437617afd83ad99ee79a
kowal319 <bartlomiej319@wp.pl>
2023-11-05 18:25:51 +0100

    cart implementation done

--- demo/src/main/java/com/example/demo/Cart.java
+++ demo/src/main/java/com/example/demo/Cart.java
@@ -4,6 +4,7 @@ package com.example.demo;
 import com.example.demo.entity.CartItem;
 import com.example.demo.entity.Product;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Setter
 @Component
 @Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Cart {
